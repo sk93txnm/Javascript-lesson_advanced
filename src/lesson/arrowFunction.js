21cec779284854162092d5e14c0ff23c01dbf0c3
ren <sk93txnm1582@gmail.com>
2026-03-11 14:28:52 +0900

    JavaScript発展_secrion7

--- src/lesson/arrowFunction.js
+++ src/lesson/arrowFunction.js
@@ -1,18 +1,34 @@
-// ここにコードを書きながら確認しましょう！
+// functionキーワードとの違い
+
+// 定義
+// const 変数名 = function(引数) {
+//   処理の内容
+// };
+
+// const 変数名 = (引数) => {
+//   処理の内容
+// };
+
+// functionキーワードを使用していた部分が、アロー関数では=>に置き換わってますね。
+// このようにアロー関数では、functionキーワードの部分を=>で書く形になります。
 
 
+// ■アロー関数の省略記法
+// 関数の引数が1つの時は()を省略できる
 
+// // 引数を1つ受け取っている
+// const arrowFn = (arg) => {
+//   console.log(arg);
+// };
 
-
-
-
-
-
-
+// // 引数が一つなので()を省略して書くことができる
+// const arrowFn = arg => {
+//   console.log(arg);
+// };
 
 
 // 練習問題
 // 問題 1
-const testFn = function(name) {
-  return `Gizumo ${name}`;
-};
+const testFn = name => `Gizumo ${name}`;
+
+console.log(testFn('Karen'));
