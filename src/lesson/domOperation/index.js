1f29aff048b9d50c4e8e37a84a64390ccfa0894d
ren <sk93txnm1582@gmail.com>
2026-03-12 08:07:23 +0900

    JavaScript発展_secrion10

--- src/lesson/domOperation/index.js
+++ src/lesson/domOperation/index.js
@@ -1,14 +1,27 @@
 import 'animate.css';
 
 const title = document.getElementById('title');
-
-
-console.log(title);
-
 const button = document.querySelector('.button');
+const eventCard = document.querySelector('.card-type--yellow');
 
-console.log(button); // <button id="button" type="button" class="button">animation</button>
+button.addEventListener('click', () => {
+  // ① DOM Operation を落下（ぶら下がり）アニメーション
+  title.classList.add('animate__hinge');
 
+
+  // ② アニメーション終了後に Event Type を表示
+  title.addEventListener(
+    'animationend',
+    () => {
+      // display: none → block に変更
+      eventCard.style.display = 'block';
+
+      // 下からフェードインするアニメーションを付与
+      eventCard.classList.add('animate__fadeInUp');
+    },
+    { once: true } // 1回だけ実行
+  );
+});
 
 
 
