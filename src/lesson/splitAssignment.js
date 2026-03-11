f0a849360bda941454b4edf3a7cc490601ee3eba
ren <sk93txnm1582@gmail.com>
2026-03-11 11:58:29 +0900

    JavaScript発展-section5

--- src/lesson/splitAssignment.js
+++ src/lesson/splitAssignment.js
@@ -1,25 +1,97 @@
-// ここにコードを書きながら確認しましょう！
+// const array = ['one', 'two', 'three'];
+// const [ hoge, fuga, piyo ] = array;
+
+// console.log(hoge); // one
+// console.log(fuga); // two
+// console.log(piyo); // three
 
 
+// function testFn() {
+//   return ['Hello', 2022];
+// }
 
+// const [text, year] = testFn();
 
+// console.log(text); // Hello
+// console.log(year); // 2022
 
+// const gizumo = {
+//   name: 'Gizumo',
+//   established: '2015年5月15日',
+//   place: 'Shibuya',
+// };
 
+// // 分割代入
+// const { name, established, place } = gizumo;
 
+// console.log(name); // Gizumo
+// console.log(established); // 2015年5月15日
+// console.log(place); // Shibuya
 
+// 変数名に別名つける
 
+// const gizumo = {
+//   name: 'Gizumo',
+//   established: '2015年5月15日',
+//   place: 'Shibuya',
+// };
+
+// // valueの部分に使用したい変数名を記述
+// const { name: companyName } = gizumo;
+
+// console.log(companyName); // Gizumo
+
+// 入れ子のオブジェクトに分割代入を使用する
+
+// const gizumo = {
+//   name: 'Gizumo',
+//   established: '2015年5月15日',
+//   place: 'Shibuya',
+//   course: {
+//     frontend: 'Vue',
+//     backend: 'Laravel',
+//   },
+// };
+
+// const { course: { frontend, backend }} = gizumo;
+
+// console.log(frontend); // Vue
+// console.log(backend); // Laravel
+
+const array = ['one','two','three'];
+const [hoge,fuga,piyo] = array;
+
+console.log(hoge);
+console.log(fuga);
+console.log(piyo);
+
+// 関数の返り値を分割代入で取得する
+
+function testFn(){
+  return ['Hello',2022];
+}
+
+const [text,year] = testFn();
+
+console.log(text);
+console.log(year);
 
 
 
 // 練習問題
 // 問題 1
+
 function arrayFn(arg) {
   return ['jQuery', 'Vue', arg];
 }
 
+const [userjQuery, userVue, userarg] = arrayFn('React');
+
+console.log(userarg); // React
+
 // 問題 2
-function objectFn() {
-  console.log();
+function objectFn({name}) {
+  console.log(name);
 }
 
 const argObject = {
